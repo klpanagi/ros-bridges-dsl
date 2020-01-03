36665207c70f177bc2b80715268116823814a431
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-01-03 21:24:06 +0200

    .

--- gen_ros_amqp_bridges.py
+++ gen_ros_amqp_bridges.py
@@ -6,10 +6,14 @@ from os import mkdir, chmod
 
 import shutil
 
+import yaml
+
 import jinja2
-from textx import metamodel_from_file
 
 from _logging import create_logger
+
+TEMPLATE_DIR_PATH = join(dirname(__file__), 'templates')
+GEN_DIR_PATH = join(TEMPLATE_DIR_PATH, '..', 'gen')
 
 
 class ROSPackageCfg(object):
@@ -25,6 +29,17 @@ class ROSPackageCfg(object):
         self.version = version
         self.rosdeps = rosdeps
 
+def _mkdir(self, destdir):
+    # Create output folder
+    if not exists(destdir):
+        mkdir(destdir)
+
+
+def _cpdir(self, src, dest):
+    if exists(dest) and isdir(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
+
 
 class Generator(object):
     def __init__(self):
@@ -37,51 +52,11 @@ class Generator(object):
         """Abstract. Implement."""
         raise NotImplementedError()
 
-    def _mkdir(self, destdir):
-        # Create output folder
-        if not exists(destdir):
-            mkdir(destdir)
-
-    def _cpdir(self, src, dest):
-        if exists(dest) and isdir(dest):
-            shutil.rmtree(dest)
-        shutil.copytree(src, dest)
-
     def _init_tpl_engine(self):
         self.jinja_env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(self.this_dir),
             trim_blocks=True,
             lstrip_blocks=True)
-
-    def _create_ros_py_pkg(self, package):
-        ros_py_pkg_tpl = join(self.tpl_dir, 'ros_py_pkg_tpl')
-        pkg_root = join(self.gen_dir, package.name)
-        self._mkdir(pkg_root)
-        self._mkdir(join(pkg_root, 'src'))
-        self._mkdir(join(pkg_root, 'launch'))
-        self._mkdir(join(pkg_root, 'scripts'))
-        self._mkdir(join(pkg_root, 'cfg'))
-
-        cmake_tpl = join(ros_py_pkg_tpl, 'CMakeLists.tpl.txt')
-        pkg_xml_tpl = join(ros_py_pkg_tpl, 'package.tpl.xml')
-        setup_tpl = join(ros_py_pkg_tpl, 'setup.tpl.py')
-
-        tpl = self.jinja_env.get_template(cmake_tpl)
-        gen_path = join(pkg_root, 'CMakeLists.txt')
-        with open(gen_path, 'w') as f:
-            f.write(tpl.render(package=package))
-
-        tpl = self.jinja_env.get_template(pkg_xml_tpl)
-        gen_path = join(pkg_root, 'package.xml')
-        with open(gen_path, 'w') as f:
-            f.write(tpl.render(package=package))
-
-        tpl = self.jinja_env.get_template(setup_tpl)
-        gen_path = join(pkg_root, 'setup.py')
-        with open(gen_path, 'w') as f:
-            f.write(tpl.render(package=package))
-
-        return pkg_root
 
     def _gen_launch_file(self, package, bridges, launcher_name='launcher'):
         ros_py_pkg_tpl = join(self.tpl_dir, 'ros_py_pkg_tpl')
@@ -211,13 +186,69 @@ class RosAMQPBridgesGen(Generator):
                 amqp_topic.uri, ros_sub.topic.uri))
 
 
+def _create_ros_py_pkg(self, package):
+    ros_py_pkg_tpl = join(TEMPLATE_DIR_PATH, 'ros_py_pkg_tpl')
+    this_dir = dirname(__file__)
+    pkg_root = join(GEN_DIR_PATH, package.name)
+    _mkdir(pkg_root)
+    _mkdir(join(pkg_root, 'src'))
+    _mkdir(join(pkg_root, 'launch'))
+    _mkdir(join(pkg_root, 'scripts'))
+    _mkdir(join(pkg_root, 'cfg'))
+
+    cmake_tpl = join(ros_py_pkg_tpl, 'CMakeLists.tpl.txt')
+    pkg_xml_tpl = join(ros_py_pkg_tpl, 'package.tpl.xml')
+    setup_tpl = join(ros_py_pkg_tpl, 'setup.tpl.py')
+
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(this_dir),
+        trim_blocks=True,
+        lstrip_blocks=True)
+
+    tpl = jinja_env.get_template(cmake_tpl)
+    gen_path = join(pkg_root, 'CMakeLists.txt')
+    with open(gen_path, 'w') as f:
+        f.write(tpl.render(package=package))
+
+    tpl = jinja_env.get_template(pkg_xml_tpl)
+    gen_path = join(pkg_root, 'package.xml')
+    with open(gen_path, 'w') as f:
+        f.write(tpl.render(package=package))
+
+    tpl = jinja_env.get_template(setup_tpl)
+    gen_path = join(pkg_root, 'setup.py')
+    with open(gen_path, 'w') as f:
+        f.write(tpl.render(package=package))
+
+    return pkg_root
+
+
+def parse_yaml(fpath):
+    with open(fpath, 'r') as f:
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
+        model = yaml.load(f, Loader=yaml.FullLoader)
+        return model
+
+
+def generate(model):
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(dirname(__file__)),
+        trim_blocks=True,
+        lstrip_blocks=True)
+
+    connectors = model['connector']
+    broker = model['connector'][0]  # Only 1 broker is currently supported
+
+    for k in connectors:
+        print(k)
+
+
 if __name__ == '__main__':
     this_folder = dirname(__file__)
     if len(sys.argv) != 2:
         print('[*] - Usage: python {} <model>'.format(sys.argv[0]))
     else:
-        meta_model = metamodel_from_file(join(this_folder,
-                                              'ros-amqp-metamodel.tx'))
-        model = meta_model.model_from_file(sys.argv[1])
-        gen = RosAMQPBridgesGen()
-        gen.generate(model)
+        fpath = sys.argv[1]
+        model = parse_yaml(fpath)
+        generate(model)
